d87a784fe3012f7832b719daecf2670142cdeef9
Kevin Jay <kjayh@unal.edu.co>
2024-04-09 20:15:30 -0500

    configuraciones para testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+//  <reference types="Vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
@@ -5,6 +8,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
